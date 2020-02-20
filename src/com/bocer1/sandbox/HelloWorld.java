0958379aa0da5ef589ddf1ec8e9a9366ddfa27f6
bocer1 <crypto_algo@yahoo.com>
2020-02-20 20:56:59 +0800

    Added ellipses

--- src/com/bocer1/sandbox/HelloWorld.java
+++ src/com/bocer1/sandbox/HelloWorld.java
@@ -2,6 +2,6 @@ package com.bocer1.sandbox;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to github");
+        System.out.println("Welcome to github...");
     }
 }
