4ffa9d81877c1c615492629cf577e203c8590d0e
bocer1 <crypto_algo@yahoo.com>
2020-02-20 21:44:41 +0800

    Add constant string for welcome message

--- src/com/bocer1/sandbox/Printer.java
+++ src/com/bocer1/sandbox/Printer.java
@@ -1,6 +1,8 @@
 package com.bocer1.sandbox;
 
 public class Printer {
+    private static final String WELCOME_MSG = "Welcome to github with authentication enabled";
+    
     public static String getMessage() {
         return "Welcome to github with authentication enabled";
     }
