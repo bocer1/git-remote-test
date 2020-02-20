6dd56fa4c9b0c305b248a122cf71c0d8b3260033
bocer1 <crypto_algo@yahoo.com>
2020-02-21 02:38:31 +0800

    Implemented Util.add()

--- src/com/bocer1/sandbox/Util.java
+++ src/com/bocer1/sandbox/Util.java
@@ -1,4 +1,16 @@
 package com.bocer1.sandbox;
 
+import java.math.BigDecimal;
+
 public class Util {
+    public static int add(Integer... addends) {
+        int sum = 0;
+        if(addends != null && addends.length > 0) {
+            for (int addend : addends) {
+                sum += addend;
+            }
+        }
+
+        return sum;
+    }
 }
